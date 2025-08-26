5cd9d951534431e77f3b08fa30172c23cc4b5c7c
Facundo Gayoso <facusbrr@gmail.com>
2025-08-26 12:57:53 -0300

    lo intente, todo culpa del ts-node

--- src/models/class.usuario.ts
+++ src/models/class.usuario.ts
@@ -1,5 +1,13 @@
 import type { IUsuario } from "../interfaces/usuario.interface.js";
+import type { Material } from "./class.material.js";
 
 class Usuario implements IUsuario {
+  constructor(
+    public readonly id: number,
+    public readonly nombre: string,
+    private readonly material: string,
+  )
+  prestar(material: Material): void {
 
+  }
 }
